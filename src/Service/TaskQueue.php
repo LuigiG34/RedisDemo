cd5afed8fd02f05c948e3a76437bed644c34f2ff
Luigi Gandemer <luigigandemer6@gmail.com>
2025-08-30 16:49:29 +0200

    Dispatch tasks to redis and process them

--- src/Service/TaskQueue.php
+++ src/Service/TaskQueue.php
@@ -4,24 +4,44 @@ namespace App\Service;
 
 use App\Enum\QueuePriority;
 use App\Message\TaskProcessMessage;
+use App\Repository\TaskRepository;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Messenger\Stamp\TransportNamesStamp;
 
 final class TaskQueue
 {
-    public function __construct(private MessageBusInterface $bus) {}
+    public function __construct(
+        private MessageBusInterface $bus,
+        private TaskRepository $repo
+    ) {}
 
-    public function enqueueProcess(int $taskId, QueuePriority $priority = QueuePriority::NORMAL): void
+    /** Dispatch a task by reading its priority from DB and mapping to the right transport. */
+    public function enqueueFromDb(int $taskId): void
     {
-        $transport = match ($priority) {
-            QueuePriority::HIGH   => 'async_high',
-            QueuePriority::LOW    => 'async_low',
-            default               => 'async',
-        };
+        $task = $this->repo->find($taskId);
+        if (!$task) {
+            return;
+        }
 
+        $enum = QueuePriority::fromInt((int) ($task->getPriority() ?? 2));
         $this->bus->dispatch(
             new TaskProcessMessage($taskId),
-            [new TransportNamesStamp([$transport])]
+            [new TransportNamesStamp([$enum->transport()])]
         );
+    }
+
+    /** Bulk version if you already have rows like [['id'=>1,'priority'=>2], ...] */
+    public function enqueueManyByRows(array $rows): void
+    {
+        foreach ($rows as $row) {
+            $id   = (int) $row['id'];
+            $prio = (int) ($row['priority'] ?? 2);
+            $enum = QueuePriority::fromInt($prio);
+
+            $this->bus->dispatch(
+                new TaskProcessMessage($id),
+                [new TransportNamesStamp([$enum->transport()])]
+            );
+        }
     }
 }
